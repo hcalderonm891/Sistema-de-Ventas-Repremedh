655038a11cc65cc77cf7167e58cb6e1b4bf5f567
HECTOR C <realhectorcalderonm@hotmail.com>
2021-10-07 23:49:27 -0600

    version 1

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2021 <a href="https://www.repremedh.com/" target="_blank">DROGUERIA REPREMEDH</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
