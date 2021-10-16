f8657ffed205c0311592a7527eab68637c61b132
hcalderonm891 <92142887+hcalderonm891@users.noreply.github.com>
2021-10-16 11:10:41 -0600

    Update factura.php

--- extensiones/tcpdf/pdf/factura.php
+++ extensiones/tcpdf/pdf/factura.php
@@ -63,7 +63,7 @@ $bloque1 = <<<EOF
 		
 		<tr>
 			
-			<td style="width:150px"><img src=""></td>
+			<td style="width:150px"><img src="images/logo-negro-bloque.png"></td>
 
 			<td style="background-color:white; width:140px">
 				
